fa3d46bc64db680966bd5995fde2fb985917951d
Rotoan <85401944+Rotoan@users.noreply.github.com>
2021-07-04 09:44:00 -0600

    Add files via upload
    updated README

--- PyPoll.py
+++ PyPoll.py
@@ -5,6 +5,7 @@ import os
 #1. data we need to retrieve
 ###open file and load election data
 File_to_load = ('Resources/election_results.csv')
+File_to_save = os.path.join("analysis", "election_analysis.txt")
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
@@ -23,25 +24,35 @@ with open(File_to_load, "r") as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
+        
+with open(File_to_save, "w") as txt_file:
+    election_results = (
+        f'\nElection Results\n'
+        f'\n-------------------------\n'
+        f'\nTotal Votes: {total_votes: ,}\n'
+        f'\n-------------------------\n'
+    )
+    txt_file.write(election_results)
+    print(election_results, end ='')
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = round(float(votes) / float(total_votes) * 100, 1)
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
-        winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"Winning Vote Count: {winning_count:,}\n"
-        f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
-        print(winning_candidate_summary)
+            winning_candidate_summary = (
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
+    txt_file.write(winning_candidate_summary)
+    print(winning_candidate_summary)
         
-
-### close file
-
-#                election_data.close()
 
 #2. total number of votes cast
 #3. complete list of candidates that received votes
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Election_Analysis
 DU Module 3, election analysis using python in VS Code
+
+# Objectives
+## 1. Total number of votes cast
+## 2. A complete list of candidates who received votes
+## 3. Total number of votes each candidate received
+## 4. Percentage of votes each candidate won
+## 5. The winner of the election based on popular vote
+
+# Resources
+-Data Source: election_results.csv
+-Software: Python 3.7.6
+
+# Results
+Winner: Diana DeGette with a vote count of 272,892 which is 73.8% of total votes
+
+#Challenges
+Data does not break down types of votes, this analysis completely relies on the accuracy of the raw vote count resource.
