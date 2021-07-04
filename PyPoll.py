4e73f6511c4755d39ec139ecc5da11c504afad5a
Rotoan <85401944+Rotoan@users.noreply.github.com>
2021-07-04 05:20:50 -0600

    Add files via upload
    added raw data Resources folder, PyPoll.py and PyPractice.py

---
+++ PyPoll.py
@@ -0,0 +1,18 @@
+#1. data we need to retrieve
+###open file and load election data
+File_to_load = ('Resources/election_results.csv')
+#             election_data = open(File_to_load, 'r')
+with open(File_to_load) as election_data:
+### perform analysis
+    print(election_data)
+### close file
+
+#                election_data.close()
+
+#2. total number of votes cast
+#3. complete list of candidates that received votes
+#4. vote totals per candidate
+#5. percentage vote total for all candidates
+#6. election winner based on popular vote
+
+
---
+++ Python_practice.py
@@ -0,0 +1,16 @@
+import csv
+import os
+#load election results raw data from csv
+File_to_load = os.path.join("Resources", "election_results.csv")
+with open(File_to_load, "r") as election_data:
+    file_reader = csv.reader(election_data)
+    #for row in file_reader: 
+    headers = next(file_reader)
+    print(headers)
+
+
+#File_to_save =  os.path.join("Analysis", "election_analysis.txt")
+#with open(File_to_save, "w") as txt_file:
+#    txt_file.write("hello world!")
+
+#open output text document in write mode
